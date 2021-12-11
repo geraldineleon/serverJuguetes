c4919ff3de4955e6fef0a7fa540846a826c01831
Daniela Tellez <deysigeraldine.leon@utxicotepec.edu.mx>
2021-12-11 03:55:44 -0600

    asi es

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//Configurar cabeceras para CORS
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY,Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Allow", "GET,POST,PUT,DELETE,OPTIONS");
+  next();
+});
+
+app.get("/", (req, res) => {
+  /* res.send("Hello world!"); */
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get("/api/juguetes/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/juguetes.json"));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor en: ", process.env.PORT || 3000);
+});
